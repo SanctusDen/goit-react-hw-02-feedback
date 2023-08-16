5cd61f96f24c764a18d8b5eecd025839e79c19ab
Dionisii <121027564+SanctusDen@users.noreply.github.com>
2023-08-16 17:16:34 +0200

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,58 @@
 import React from "react";
-// import Counter from './Counter/Counter.js'
 
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-    </div>
-  );
+import { FeedbackOption } from 'components/FeedbackOptions/FeedbackOptions';
+import { Section } from '../Section/Section';
+import { Notification } from "components/Notification/Notification";
+import { Statistics } from "components/Statistacs/Statistics";
+
+class App extends React.Component{
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
+    
+
+    // handleFeedback = e => {
+    //     this.setState((prevState) => {
+    //         return {
+    //             [option]: prevState[option] + 1,
+    //         };
+    //     });
+    // };
+    
+    onLeaveFeedback = (evt) => {
+      
+    };
+    
+    countPositiveFeedback = () => {
+        return Math.round((this.state.good / this.TotalFeedback()) * 100)
+    }
+    
+    
+    TotalFeedback = () => {
+        const { good, neutral, bad } = this.state;
+        const total = good + neutral + bad;
+        return total;
+    };
+
+    render() {
+        const { good, neutral, bad } = this.state;
+        const onFeedbackOptions = Object.keys(this.state);
+        const positivePercentage = this.countPositiveFeedback();
+        const countTotal = this.TotalFeedback();
+        
+    return (
+      <>
+        <Section title="Please leave your feedback here">
+        <FeedbackOption options={onFeedbackOptions} onLeaveFeedBack={this.onLeaveFeedback} />
+        {countTotal > 0 ? (<Statistics good={good} neutral={neutral} bad={bad} total={countTotal} positivePercentage={positivePercentage} />) : 
+          (<Notification message="There is no feedback" />)
+        }
+        </Section>
+      </>
+    );
+  }
 };
+
+export default App;
---
+++ src/components/Statistacs/Statistics.jsx
@@ -0,0 +1,5 @@
+export const Statistics = () => {
+    return (
+        <div>This Static</div>
+    )
+}
